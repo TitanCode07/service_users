9dba71f603787ed8ebb66425158aabe5699776d2
iralak07 <https://github.com/Iralak07>
2024-10-09 16:03:37 -0300

    cors

--- app/main.py
+++ app/main.py
@@ -1,17 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.database import engine, SessionLocal
 from app.users.models import Base
 from app.users import routers
 from app.auth_user import routes as auth_routers
 
-
+# Crear las tablas
 Base.metadata.create_all(bind=engine)
-
 
 app = FastAPI()
 
+# Configurar CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir solicitudes desde cualquier origen
+    allow_credentials=True,  # Habilitar el envío de credenciales (si es necesario)
+    allow_methods=["*"],  # Permitir todos los métodos (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Permitir cualquier encabezado
+)
 
+# Incluir routers
 app.include_router(routers.router)
 app.include_router(auth_routers.router)
+
 
 
